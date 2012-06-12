d9e2122ab8179c8f2a29eb69625f5f7f5509fcd5
HAYASI Hideki <linxs@linxs.org>
2012-06-12 22:49:56 +0900

    Bugs Fixed - bitmap.py has been missed due to lack of declaration in MANIFEST.  Oops!

--- xdwlib/__init__.py
+++ xdwlib/__init__.py
@@ -22,10 +22,12 @@ from binder import Binder, create_binder
 from documentinbinder import DocumentInBinder
 from page import Page, PageCollection
 from annotation import Annotation
+from bitmap import Bitmap
+
 
 __author__ = "HAYASI Hideki"
 __copyright__ = "Copyright (C) 2010 HAYASI Hideki <linxs@linxs.org>"
 __license__ = "ZPL 2.1"
-__version__ = "2.13.7"
+__version__ = "2.13.8"
 __email__ = "linxs@linxs.org"
 __status__ = "Development"
