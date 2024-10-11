47855f4afdbf42f0e905e107c008b28dfd3686d4
HAYASHI,Hideki <hideki@hayasix.com>
2024-10-11 18:27:58 +0900

    Other Changes - Changed document theme on Readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'python_docs_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
